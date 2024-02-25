b791857b7df1fd6d43daea19d593f742dc3c708f
@NISHAN <140871999+ASHADEEP-NISHAN@users.noreply.github.com>
2024-02-25 12:46:16 +0530

    Update rpc.py

--- rpc.py
+++ rpc.py
@@ -2,21 +2,24 @@ import random as r
 a=['rock','paper','scissor']
 b=r.choice(a)
 
-c=input("enter [rock/paper/scissor]")
-if b==c:
-    print("tie")
-elif b=='rock' and c=='paper':
-    print("you win")
-elif b=='rock' and c=='scissor':
-    print("you lose")
-elif c == 'rock' and b == 'paper':
-    print("you lose")
-elif b == 'paper'and c=='scissor':
-    print("you win")
-elif c == 'rock' and b == 'scissor':
-    print("you win")
-elif c == 'paper'and b =='scissor':
-    print("you lose")
-print("i choose::",b)
+while True:
+    c=input("enter [rock/paper/scissor]").lower()
+    print("i choose::",b)
+    if b==c:
+        print("tie")
+    elif b=='rock' and c=='paper':
+        print("you win")
+    elif b=='rock' and c=='scissor':
+        print("you lose")
+    elif c == 'rock' and b == 'paper':
+        print("you lose")
+    elif b == 'paper'and c=='scissor':
+        print("you win")
+    elif c == 'rock' and b == 'scissor':
+        print("you win")
+    elif c == 'paper'and b =='scissor':
+        print("you lose")
+    else:
+        print("invalid input")
 
 
